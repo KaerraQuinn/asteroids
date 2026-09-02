c628e7af5c3966839b504d439497aa70c5df861e
KaerraQuinn <deadmoon5150@gmail.com>
2026-09-02 15:55:09 -0600

    groups and w movement fix

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatable, drawables)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     while True:
         log_state()
@@ -17,8 +20,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
